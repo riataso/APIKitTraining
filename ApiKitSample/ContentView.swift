9200e71ab3a3fe5c327e6e13157f200963a98d27
riataso <rarararai0070@gmail.com>
2024-10-27 18:33:56 +0900

    Initial Commit

---
+++ ApiKitSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApiKitSample
+//
+//  Created by 佐藤来 on 2024/10/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ApiKitSampleTests/ApiKitSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApiKitSampleTests.swift
+//  ApiKitSampleTests
+//
+//  Created by 佐藤来 on 2024/10/27.
+//
+
+import Testing
+@testable import ApiKitSample
+
+struct ApiKitSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
